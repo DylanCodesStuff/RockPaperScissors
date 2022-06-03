8300c681a10858f17b967df7ac2bafa3c26ab811
Dylan Scriven <dscriven16@gmail.com>
2022-06-03 14:22:04 -0400

    Implement javascript to buttons.

--- myscripts.js
+++ myscripts.js
@@ -3,6 +3,11 @@ function computerPlay(){
     return Math.floor(Math.random()*3);   //gives a random number between 0,1, and 2.
 
 }
+function disableButtons(){
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
 
 let playerSelection;
@@ -10,93 +15,64 @@ let computerSelection;
 let playerWins = 0;
 let computerWins = 0;
 
-function playRound(playerSelection, computerSelection){
-   computerSelection = computerPlay();
-   playerSelection = prompt("Type Your Choice... Rock, Paper, or Scissors?");
-   playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection){
+   let computerSelection = computerPlay();
+   let result = '';
    console.log("playerSelection:" + playerSelection);
    console.log("computerSelection:" + computerSelection);
-    if(playerSelection == "rock"){
-        playerSelection = 0;
-    }
-    else if(playerSelection == "paper"){
-        playerSelection = 1;
-    }
-    else if (playerSelection == "scissors"){
-        playerSelection = 2;
-    }
-    else {
-        return "error";
-    }
+  
+   
+   if((playerSelection == 0 && computerSelection == 2) ||
+   (playerSelection == 1 && computerSelection == 0) ||
+   (playerSelection == 2 && computerSelection == 1)){
+    playerWins++;
+    result = `You win! You have ${playerWins} wins, the opponent has ${computerWins} wins.`;
+
+    if(playerWins == 5){
+        result = `You won the whole game! Nice!`;
+        disableButtons();
+        }
    
 
-    if (playerSelection == 0 && computerSelection == 1){
-        return "You Lose! Paper beats Rock";
-    }
-    else if (playerSelection == 0 && computerSelection == 2){
-        return "You Win! Rock beats Scissors";
-    }
-    else if(playerSelection == 1 && computerSelection == 0){
-        return "You Win! Paper beats Rock";
-
-    }
-    else if(playerSelection == 1 && computerSelection == 2){
-        return "You Lose! Scissors beats Paper";
-    }
-    else if(playerSelection == 2 && computerSelection == 0){
-        return "You Lose! Rock beats Scissors";
-    }
-    else if(playerSelection == 2 && computerSelection == 1){
-        return "You Win! Scissors beats Paper";
-    }
-    else if(playerSelection == computerSelection){
-        return "It's a tie! You chose the same thing!"
-    }
-    else {
-        return "error";
-    }
 }
-
-
-function game(){
-    
-    for(let i = 0; i < 5; i ++){
-        thisRound = playRound(playerSelection,computerSelection);
-        console.log(thisRound);
-        if(thisRound =="You Win! Rock beats Scissors"|| thisRound=="You Win! Paper beats Rock"|| thisRound =="You Win! Scissors beats Paper"){
-            playerWins++;
-            
-        }
-        else if(thisRound == "You Lose! Paper beats Rock"|| thisRound=="You Lose! Scissors beats Paper"|| thisRound == "You Lose! Rock beats Scissors"){
-
-            computerWins++;
+   
+    else if ((playerSelection == 2 && computerSelection ==0)||
+    (playerSelection == 1 && computerSelection == 2)||
+    (playerSelection == 0 && computerSelection == 1)){
+        computerWins++;
+        result = `You lose! You have ${playerWins} wins, the opponent has ${computerWins} wins.`
         
-        }
-        else;
-
-
-    
-    
-    }
-    if(playerWins > computerWins){
-        let finalPlayerWins = playerWins;
-        playerWins =0;
-        computerWins=0;
-        return finalPlayerWins;
+        if(computerWins == 5){
+            result = `You lose the game! Better luck next time.`;
+            disableButtons();
+            }
         
-    }
-    else if(playerWins < computerWins){
-        let finalComputerWins = computerWins;
-        playerWins =0;
-        computerWins=0;
-        return finalComputerWins;
     }
     else{
-        playerWins =0;
-        computerWins=0;
-        return "SUDDEN DEATH";
-
+        result = `Thats a tie! You chose the same thing! The score is ${playerWins} to ${computerWins}`
     }
+
     
+
+   
+
+    document.getElementById('result').innerHTML = result
+   return;
+    
+
 }
 
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+rock.addEventListener('click', function(){
+    playRound('0');
+});
+paper.addEventListener('click',function(){
+    playRound('1');
+})
+scissors.addEventListener('click',function(){
+    playRound('2');
+})
